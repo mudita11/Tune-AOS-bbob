3dec0a6f0c3e729cc6a8e8159bf0ee3e7dbd4abc
mudita11 <ms1938@york.ac.uk>
2019-02-21 10:58:29 +0000

    Reimlpemented reward, quality and selection

--- bin/aos.py
+++ bin/aos.py
@@ -15,6 +15,13 @@ def debug_print(*args, **kwargs):
 # MANUEL: What is the difference between AOS and unknown AOS?
 # MUDITA: I am referring to a combination of these components as Unknown AOS if that combination is not considered in literature.
 
+# MANUEL: where is Rec_PM and the other AOS methods you implemented for the PPSN paper?
+
+import sys
+def debug_print(*args, **kwargs):
+    print(*args, file=sys.stderr, **kwargs)
+    
+# MANUEL: What is the difference between AOS and unknown AOS?
 class AOS(object):
     def __init__(self, popsize, F1, F, u, X, f_min, x_min, best_so_far, best_so_far1, n_ops, window_size= None):
         self.popsize = int(popsize)
@@ -45,9 +52,14 @@ class AOS(object):
     
     ##################################################Offspring Metric definitions##################################################################
     def OM_Update(self):
+<<<<<<< HEAD
         """ If offspring improves over parent, Offsping Metric (OM) stores (1)fitness of offspring (2)fitness improvemnt from parent to offsping (3)fitness improvemnt from best parent to offsping (4)fitness improvemnt from best so far to offsping (5)fitness improvemnt from median parent fitness to offsping (6)relative fitness improvemnt """
         ## MANUEL: describe this function!
         ## MUDITA: If offspring improves over parent, Offsping Metric (OM) stores (1)fitness of offspring (2)fitness improvemnt from parent to offsping (3)fitness improvemnt from best parent to offsping (4)fitness improvemnt from best so far to offsping (5)fitness improvemnt from median parent fitness to offsping (6)relative fitness improvemnt
+=======
+        """ ??? """
+        ## MANUEL: describe this function!
+>>>>>>> 9689958891cbb8e0fec6df86d202c39f597b03a3
         third_dim = []
         # success = np.zeros(self.n_ops); unsuccess = np.zeros(self.n_ops)
         F_min = np.min(self.F)
@@ -63,6 +75,7 @@ class AOS(object):
             second_dim[2] = self.F[i] - self.F1[i]
             if self.F1[i] <= F_min:
                 second_dim[3] = F_min - self.F1[i]
+<<<<<<< HEAD
                 
             if self.F1[i] <= self.best_so_far:
                 second_dim[4] = self.best_so_far - self.F1[i]
@@ -72,6 +85,17 @@ class AOS(object):
                 
             second_dim[6] = (self.best_so_far / (self.F1[i] + 0.001)) * F_absdiff[i]
                 
+=======
+                
+            if self.F1[i] <= self.best_so_far:
+                second_dim[4] = self.best_so_far - self.F1[i]
+            
+            if self.F1[i] <= F_median:
+                second_dim[5] = F_median - self.F1[i]
+                
+            second_dim[6] = (self.best_so_far / (self.F1[i] + 0.001)) * F_absdiff[i]
+                
+>>>>>>> 9689958891cbb8e0fec6df86d202c39f597b03a3
             if np.any(self.window[:, 1] == np.inf):
                 for value in range(self.window_size - 1, -1, -1):
                     if self.window[value][0] == -1:
@@ -366,6 +390,7 @@ def angle_between(p1, p2):
 #    if max_gen > len(gen_window):
 #        max_gen = len(gen_window)
     # Projection on line B with thetha = pi/4
+<<<<<<< HEAD
 #    B = [1, 1]
 #    for i in range(n_ops):
 #        b = []
@@ -373,6 +398,15 @@ def angle_between(p1, p2):
 #            if np.any(gen_window[len(gen_window)-1, :, 0] == i):
 #                b.append(gen_window[j, np.where(gen_window[j, :, 0] == i) and np.where(gen_window[j, :, Off_met] != -1), Off_met])
 #        if b != []:
+=======
+    B = [1, 1]
+    for i in range(n_ops):
+        b = []
+        for j in range(len(gen_window)-1, len(gen_window)-max_gen-1, -1):
+            if np.any(gen_window[len(gen_window)-1, :, 0] == i):
+                b.append(gen_window[j, np.where(gen_window[j, :, 0] == i) and np.where(gen_window[j, :, Off_met] != -1), Off_met])
+        if b != []:
+>>>>>>> 9689958891cbb8e0fec6df86d202c39f597b03a3
             # Diversity: np.std(np.hstack(b)) and Quality: np.average(np.hstack(b))
 #            reward[i] = 1-distance.cosine([np.std(np.hstack(b)), np.average(np.hstack(b))], B); # print(b, reward[i])
 #    reward = reward - np.min(reward)
@@ -393,6 +427,7 @@ def angle_between(p1, p2):
 
 # index: 4 Applicable for fix size window (Done!)
 # Parameter(s): window, decay_reward4
+<<<<<<< HEAD
 #def Reward4(popsize, n_ops, window_size, window, gen_window, Off_met, max_gen, decay_reward3, decay_reward4, int_a_reward5, b_reward5, e_reward5, a_reward71, c_reward9, int_b_reward9, int_a_reward9,int_a_reward101, b_reward101, opu, old_opu, total_success, total_unsuccess, old_reward):
 #    reward = np.zeros(n_ops)
 #    window = window[window[:, Off_met] != -1][:, :];
@@ -407,6 +442,22 @@ def angle_between(p1, p2):
 #                reward[j] += value
 #    reward /= np.sum(reward)
 #    return reward
+=======
+def Reward4(popsize, n_ops, window_size, window, gen_window, Off_met, max_gen, decay_reward3, decay_reward4, int_a_reward5, b_reward5, e_reward5, a_reward71, c_reward9, int_b_reward9, int_a_reward9,int_a_reward101, b_reward101, opu, old_opu, total_success, total_unsuccess, old_reward):
+    reward = np.zeros(n_ops)
+    window = window[window[:, Off_met] != -1][:, :];
+    window_op_sorted, N, rank = count_op(n_ops, window, Off_met)
+    for i in range(len(window_op_sorted)):
+        value = (decay_reward4 ** rank[i]) * (window_size - rank[i])
+        ## MANUEL: If window_op_sorted only contains values from 0 to n_ops, then this loop can be simply:
+        # reward[window_op_sorted] += value
+        ## Test it!
+        for j in range(n_ops):
+            if window_op_sorted[i] == j:
+                reward[j] += value
+    reward /= np.sum(reward)
+    return reward
+>>>>>>> 9689958891cbb8e0fec6df86d202c39f597b03a3
 
 ########################Success based###########################################
 
@@ -625,6 +676,7 @@ def angle_between(p1, p2):
 #    tran_matrix = TM(n_ops, old_probability)
 #    tran_matrix = normalize_matrix(np.random.rand(n_ops, n_ops)); # print(old_probability)
 
+<<<<<<< HEAD
 #    Q = c1_quality6 * reward + c2_quality6 * old_reward
 #    if discount_rate != 0:
 #        Q = np.matmul(np.linalg.pinv(np.array(1 - discount_rate * tran_matrix)), np.array(Q))
@@ -636,6 +688,142 @@ def angle_between(p1, p2):
 
 
 ##################################################Reward definitions######################################################################
+=======
+    Q = c1_quality6 * reward + c2_quality6 * old_reward
+    if discount_rate != 0:
+        Q = np.matmul(np.linalg.pinv(np.array(1 - discount_rate * tran_matrix)), np.array(Q))
+        Q = Q - np.max(Q)
+        Q = np.exp(Q)
+        # Normalisation for all operators
+        Q = Q / np.sum(Q)
+    return Q
+
+##################################################Probability definitions######################################################################
+
+from abc import ABC,abstractmethod
+
+def build_probability(choice, n_ops, prob_args):
+    if choice == 0:
+        return Probability0(n_ops, prob_args["p_min_prob"], prob_args["e_prob"])
+    elif choice == 1:
+        return Probability1(n_ops, prob_args["p_min_prob"], prob_args["p_max_prob"], prob_args["beta_prob"])
+    elif choice == 2:
+        return Probability2(n_ops, prob_args["p_min_prob"], prob_args["beta_prob"])
+    elif choice == 3:
+        return Probability3(n_ops)
+    else:
+        raise ValueError("choice {} unknown".format(choice))
+ 
+class ProbabilityType(ABC):
+    def __init__(self, n_ops, p_min_prob = None, beta_prob = None):
+        self.p_min_prob = p_min_prob
+        self.beta_prob = beta_prob
+        self.old_probability = np.full(n_ops, 1.0 / n_ops)
+        self.eps = np.finfo(self.old_probability.dtype).eps
+
+    def check_probability(self, probability):
+        probability /= np.sum(probability)
+        assert np.allclose(np.sum(probability), 1.0, equal_nan = True)
+        assert np.all(probability >= -0.0)
+        # MANUEL: This is wrong! It creates a view of an array and not a copy
+        # self.old_probability = self.probability
+        self.old_probability[:] = probability
+        return(probability)
+
+    @abstractmethod
+    def calc_probability(self, quality):
+        "Must be implemented by derived probability methods"
+        pass
+    
+# MANUEL: These should have more descriptive names and a doctstring documenting
+# where they come from (references) and what they do.
+class Probability0(ProbabilityType):
+    def __init__(self, n_ops, p_min_prob = 0.1, e_prob = 0.0):
+        super().__init__(n_ops, p_min_prob)
+        # np.finfo(np.float32).eps adds a small epsilon number that doesn't make any difference but avoids 0.
+        self.e_prob = e_prob + self.eps
+        # MANUEL: Please do this in every class so one can debug what is actually running.
+        debug_print("\n {} : p_min_prob = {}, e_prob = {}".
+                    format(type(self).__name__, self.p_min_prob, self.e_prob))
+        
+    def calc_probability(self, quality):
+        probability = self.p_min_prob + (1 - len(quality) * self.p_min_prob) * \
+                      (quality + self.e_prob) / np.sum(quality + self.e_prob)
+        return super().check_probability(probability)
+        
+
+class Probability1(ProbabilityType):
+    def __init__(self, n_ops, p_min_prob = 0.1, p_max_prob = 0.9, beta_prob = 0.1):
+        super().__init__(n_ops, p_min_prob, beta_prob = beta_prob)
+        self.p_max_prob = p_max_prob
+        
+    def calc_probability(self, quality):
+        delta = np.full(quality.shape[0], self.p_min_prob)
+        delta[np.argmax(quality)] = self.p_max_prob
+        probability = self.old_probability + self.beta_prob  * (delta - self.old_probability)
+        probability += self.eps
+        return super().check_probability(probability)
+
+class Probability2(ProbabilityType):
+    def __init__(self, n_ops, p_min_prob = 0.025, beta_prob = 0.5):
+        super().__init__(n_ops, p_min_prob, beta_prob = beta_prob)
+        
+    def calc_probability(self, quality):
+        
+        # MANUEL: Why do this?
+        if np.sum(quality) != 0:
+            quality = quality.copy()
+            # Normalize
+            quality += self.eps
+            quality /= np.sum(quality)
+
+        # np.maximum is element-wise
+        probability = self.beta_prob * np.maximum(self.p_min_prob, quality) + (1.0 - self.beta_prob) * self.old_probability[i]
+        probability += self.eps
+        return super().check_probability(probability)
+
+class Probability3(ProbabilityType):
+    def __init__(self, n_ops, ):
+        super().__init__(n_ops)
+        
+    def calc_probability(self, quality):
+        probability = quality + self.eps
+        return super().check_probability(probability)
+    
+        
+# # Parameters: p_min_prob0, e_prob0
+# def Probability0(n_ops, quality, p_min_prob0, e_prob0, p_min_prob1, p_max_prob1, beta_prob1, p_min_prob2, beta_prob2, old_probability):
+#     probability = p_min_prob + (1 - len(quality) * p_min_prob0) * ((quality + e_prob0 + np.finfo(np.float32).eps) / (np.sum(quality + e_prob0 + np.finfo(np.float32).eps)))
+#     probability = probability/np.sum(probability)
+#     return(probability)
+
+# Parameters: beta_prob1, p_min_prob1, p_max_prob1
+# def Probability1(n_ops, quality, p_min_prob0, e_prob0, p_min_prob1, p_max_prob1, beta_prob1, p_min_prob2, beta_prob2, old_probability):
+#     probability = np.zeros(n_ops)
+#     probability = old_probability + beta_prob1 * (p_min_prob1 - old_probability)
+#     probability[np.argmax(quality)] = old_probability[np.argmax(quality)] + beta_prob1 * (p_max_prob1 - old_probability[np.argmax(quality)])
+#     return ((probability + np.finfo(np.float32).eps) / (np.sum(probability + np.finfo(np.float32).eps)))
+
+# # Parameters: beta_prob2, p_min_prob2
+# def Probability2(n_ops, quality, p_min_prob0, e_prob0, p_min_prob1, p_max_prob1, beta_prob1, p_min_prob2, beta_prob2, old_probability):
+#     probability = np.zeros(n_ops)
+#     #print("qual: ",quality)
+#     for i in range(n_ops): # Direct way??????
+#         if np.sum(quality) != 0:
+#             probability[i] = beta_prob2 * np.max([p_min_prob2, np.array(quality[i] + np.finfo(np.float32).eps) / (np.sum(quality + np.finfo(np.float32).eps))]) + (1 - beta_prob2) * np.array(old_probability[i])
+#         else:
+#             probability[i] = beta_prob2 * np.max([p_min_prob2, np.array(quality[i])]) + (1 - beta_prob2) * np.array(old_probability[i])
+#     #print(probability / np.sum(probability))
+#     return ((probability + np.finfo(np.float32).eps) / (np.sum(probability + np.finfo(np.float32).eps)))
+
+# def Probability3(n_ops, quality, p_min_prob0, e_prob0, p_min_prob1, p_max_prob1, beta_prob1, p_min_prob2, beta_prob2, old_probability):
+#     probability = np.zeros(n_ops)
+#     Probability = quality
+#     return ((probability + np.finfo(np.float32).eps) / (np.sum(probability + np.finfo(np.float32).eps)))
+    
+                              
+##################################################Selection definitions######################################################################
+>>>>>>> 9689958891cbb8e0fec6df86d202c39f597b03a3
 
 from abc import ABC,abstractmethod
 
@@ -757,6 +945,7 @@ class Reward1(RewardType):
             reward = (reward / np.sum(reward))
         return -reward
 
+<<<<<<< HEAD
 
 class Reward2(RewardType):
     def __init__(self, n_ops, max_gen = 4):
@@ -784,6 +973,27 @@ class Reward2(RewardType):
 class Reward3(RewardType):
     def __init__(self, n_ops, W = 50, decay_reward3 = 0.4):
         super().__init__(n_ops, W)
+=======
+R_list = [Reward0, Reward1, Reward2, Reward3, Reward4, Reward5, Reward6, Reward70, Reward71, Reward8, Reward9, Reward101]
+Q_list = [Quality0, Quality1, Quality2, Quality3, Quality4, Quality5]
+#P_list = [Probability0, Probability1, Probability2, Probability3]
+S_list = [Selection0, Selection1]
+
+class Unknown_AOS(AOS):
+    def __init__(self, popsize, F1, F, u, X, f_min, x_min, best_so_far, best_so_far1, Off_met, Rewar, Qual, Select, n_ops, adaptation_rate, phi, max_gen, scaling_factor, c1_quality6, c2_quality6, discount_rate, delta, decay_reward3, decay_reward4, int_a_reward5, b_reward5, e_reward5, a_reward71, c_reward9, int_b_reward9, int_a_reward9, int_a_reward101, b_reward101, window_size, prob_choice, prob_args):
+        # print("Inside unknown AOS-init before super")
+        super(Unknown_AOS,self).__init__(popsize, F1, F, u, X, f_min, x_min, best_so_far, best_so_far1, n_ops, window_size)
+        # print("Rewar= ",Rewar)
+        self.Off_met = Off_met
+        # self.Select = Select
+        self.adaptation_rate = adaptation_rate
+        self.phi = phi
+        self.max_gen = max_gen
+        self.scaling_factor = scaling_factor
+        self.discount_rate = discount_rate
+        self.delta = delta
+        
+>>>>>>> 9689958891cbb8e0fec6df86d202c39f597b03a3
         self.decay_reward3 = decay_reward3
         debug_print("\n {} : W = {}, decay_reward3 = {}".
                     format(type(self).__name__, self.W, self.decay_reward3))
@@ -1009,6 +1219,7 @@ class QualityType(ABC):
         self.delta = delta
         self.c1_quality6 = c1_quality6
         self.c2_quality6 = c2_quality6
+<<<<<<< HEAD
         self.gamma = gamma
     
     def check_quality(self, quality):
@@ -1383,3 +1594,46 @@ class Unknown_AOS(AOS):
 #        SI = self.Selection_fun(self.op_init_list, self.probability)
 #        return SI
 
+=======
+        
+        self.reward = np.zeros(self.n_ops); self.old_reward = self.reward
+        self.quality = np.zeros(self.n_ops); self.quality[:] = 1.0; self.old_quality = self.quality
+        self.Reward_fun = R_list[Rewar]
+        self.Quality_fun = Q_list[Qual]
+
+        # self.probability = np.zeros(self.n_ops); self.probability[:] = 1.0 / len(self.probability)
+        self.probability = np.full(n_ops, 1.0 / n_ops)
+        self.probability_type = build_probability(prob_choice, n_ops, prob_args)
+
+        self.Selection_fun = S_list[Select]
+
+        
+        
+    def Reward(self):
+        
+        self.reward = self.Reward_fun(self.popsize, self.n_ops, self.window_size, self.window, self.gen_window, self.Off_met, self.max_gen, self.decay_reward3, self.decay_reward4, self.int_a_reward5, self.b_reward5, self.e_reward5, self.a_reward71, self.c_reward9, self.int_b_reward9, self.int_a_reward9, self.int_a_reward101, self.b_reward101, self.opu, self.old_opu, self.total_success, self.total_unsuccess, self.old_reward)
+        #print("reward ",self.reward)
+
+    def Quality(self):
+        old_probability = self.probability_type.old_probability
+        self.quality = self.Quality_fun(self.n_ops, self.adaptation_rate, self.reward, self.old_quality, self.phi, self.window, self.scaling_factor, self.c1_quality6, self.c2_quality6, self.discount_rate, self.delta, self.Off_met, old_probability, self.old_reward); # print("qual: ", self.quality)
+        # MANUEL: This is all wrong because they create views, not copies
+        self.old_reward = self.reward
+        self.old_quality = self.quality
+        #print("quality ",self.quality)
+        #assert np.sum(self.quality) > 0
+
+    # def Probability(self):
+    #     assert len(self.quality) == len(self.probability); # print("p,op1", self.probability, self.old_probability)
+    #     self.probability = self.probability_fun(self.n_ops, self.quality, self.p_min_prob0, self.e_prob0, self.p_min_prob1, self.p_max_prob1, self.beta_prob1, self.p_min_prob2, self.beta_prob2, self.old_probability);
+    #     #print("current and old probability ", self.probability, self.old_probability)
+    #     assert np.allclose(np.sum(self.probability), 1.0, equal_nan = True)
+    #     assert np.all(self.probability >= -0.0)
+    #     self.old_probability = self.probability
+
+    def Selection(self):
+        # print(self.probability, len(self.probability))
+        SI = self.Selection_fun(self.op_init_list, self.probability)
+        return SI
+
+>>>>>>> 9689958891cbb8e0fec6df86d202c39f597b03a3
