b34a0165f44030be8ea1cba1f92cbbd7d2ab1af0
MLopez-Ibanez <2620021+MLopez-Ibanez@users.noreply.github.com>
2019-03-15 10:03:16 +0000

    Print command to help debugging in case of error

--- target-runner-hv.py
+++ target-runner-hv.py
@@ -47,7 +47,6 @@ instance = sys.argv[4]
 cand_params = sys.argv[5:]
 
 #cand_params = ['--FF', '0.3', '--CR', '0.2', '--NP', '50', '--OM_choice', '1', '--rew_choice', '0', '--qual_choice', '2', '--prob_choice', '0', '--select_choice', '1', '--fix_appl', '10', '--adaptation_rate', '0.57', '--p_min', '0.05', '--error_prob','0.1']
-print(cand_params)
 
 #cand_params=[str(c1), str(c2), str(c3), str(c4), str(c5), str(c6), str(c7), str(c8), str(c9), str(c10), str(c11), str(c12), str(c13), str(c14), str(c15), str(c16), str(c17), str(c18), str(c19), str(c20), str(c21), str(c22), str(c23), str(c24), str(c25), str(c26), str(c27), str(c28), str(c29), str(c30), str(c31), str(c32)]
 #print("cand_params", cand_params)
@@ -68,16 +67,19 @@ command = " ".join([exe, "-i", instance, "--seed", seed, "--trace", trace_file] 
 
 outf = open(out_file, "w")
 errf = open(err_file, "w")
+print(command, file=errf)
 return_code = subprocess.call(command, shell=True, stdout = outf, stderr = errf)
 outf.close()
 errf.close()
 
 if return_code != 0:
+    print(command)
     now = datetime.datetime.now()
     print(str(now) + " error: command returned code " + str(return_code))
     sys.exit(1)
 
 if not os.path.isfile(out_file):
+    print(command)
     now = datetime.datetime.now()
     print(str(now) + " error: output file "+ out_file  +" not found.")
     sys.exit(1)
