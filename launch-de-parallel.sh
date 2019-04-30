3c27b76f0642526e1af6225dd92d0955296d8bc9
MLopez-Ibanez <2620021+MLopez-Ibanez@users.noreply.github.com>
2019-04-30 15:07:53 +0100

    launch-de-parallel.sh

---
+++ launch-de-parallel.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+
+fes=500000
+file="configurations.txt"
+
+de="./bin/DE_AOS.py bbob $fes 1 1"
+
+# MANUEL: where is the output saved?
+# Also, if you read the command-line parameters from a file, one per line, you can do something like:
+# cat file | xargs -n 1 $de
+# See http://man7.org/linux/man-pages/man1/xargs.1.html
+cat $file | xargs -I{} -n 1 echo $de {}
+# And if the computer you use has the 'parallel' command, then you can run in parallel with:
+# cat file | parallel $de
+# See https://www.gnu.org/software/parallel/man.html#EXAMPLE:-Working-as-xargs--n1.-Argument-appending
+
+
