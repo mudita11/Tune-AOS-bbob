231ee89ba1020f5a3e6ac58f165732c75b928fcf
MLopez-Ibanez <2620021+MLopez-Ibanez@users.noreply.github.com>
2019-04-30 15:12:56 +0100

    launch-de-parallel.sh: use parallel.

--- launch-de-parallel.sh
+++ launch-de-parallel.sh
@@ -10,7 +10,8 @@ de="./bin/DE_AOS.py bbob $fes 1 1"
 # Also, if you read the command-line parameters from a file, one per line, you can do something like:
 # cat file | xargs -n 1 $de
 # See http://man7.org/linux/man-pages/man1/xargs.1.html
-cat $file | xargs -I{} -n 1 echo $de {}
+# cat $file | xargs -I{} -n 1 echo $de {}
+cat $file | parallel -j 6 -n 1 echo $de {}
 # And if the computer you use has the 'parallel' command, then you can run in parallel with:
 # cat file | parallel $de
 # See https://www.gnu.org/software/parallel/man.html#EXAMPLE:-Working-as-xargs--n1.-Argument-appending
